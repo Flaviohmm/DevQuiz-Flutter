e862aa593c93ad09554619c0a3a8b2f8817cd417
Flaviohm2 <fhenrique609@gmail.com>
2021-04-19 22:39:24 -0300

    NLW#5 - Adicionando Logo e Gradiente

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,6 @@
+import 'package:dev_quiz/core/app_widget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'DevQuiz',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text("NLW5 Flutter"))
-        ),
-        body: Center(
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.purple,
-              borderRadius: BorderRadius.circular(10)
-            ),
-            child: Text("Olá, Gabul DEV", style: TextStyle(fontSize: 30, color: Colors.white))
-          )
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.add),
-        ),
-      ),
-    );
-  }
+  runApp(AppWidget());
 }
